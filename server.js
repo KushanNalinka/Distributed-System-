eb56967bb1ab0b95f1c6f7b36661ec52caa723e9
Kushan Nalinka <kushannalinka572@gmail.com>
2024-05-08 12:37:20 +0530

    Authentication and course
    Auth and course cruds

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import courseRoute from "./routes/course.js";
+import cookieParser from "cookie-parser";
+
 
 const app = express();
 dotenv.config();
@@ -15,6 +19,24 @@ const connect = async () => {
 };
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
+
+app.use("/api/course", courseRoute);
+
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+      success: false,
+      status: errorStatus,
+      message: errorMessage,
+      stack: err.stack,
+    });
+  });
+
 
 app.listen(8800, () => {
     connect();
